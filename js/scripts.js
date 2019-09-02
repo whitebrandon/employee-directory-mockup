201737c0e7ba1f9fce2022ae74452908980769ab
Brandon White <white.brandonsean@gmail.com>
2019-09-02 02:09:16 -0400

    Refactored using jQuery | Added right and left arrow functionality to move through modal

--- js/scripts.js
+++ js/scripts.js
@@ -6,25 +6,100 @@
     }
   }); */
 'use strict';
-    // ↓ Creates search bar
-    const searchContainer = document.querySelector('.search-container');
-    const search = document.createElement('form');
-    const searchInput = document.createElement('input');
-    const submitInput = document.createElement('input');
-    searchContainer.appendChild(search);
-    search.setAttribute('action', "#");
-    search.setAttribute('method', "get");
-    search.appendChild(searchInput);
-        searchInput.setAttribute('type', "search");
-        searchInput.setAttribute('id', "search-input");
-        searchInput.setAttribute('class', "search-input");
-        searchInput.setAttribute('placeholder', "Search...");
-    search.appendChild(submitInput);
-        submitInput.setAttribute('type', "submit");
-        submitInput.setAttribute('value', "🔍");
-        submitInput.setAttribute('id', "search-submit");
-        submitInput.setAttribute('class', "search-submit");
 
+    /**
+     * Appends child elements to parent element
+     * @param {Element} parent Parent node
+     * @param  {...any} children An array of child elements to be appended
+     */
+    const appendMultiples = (parent, ...children) => children.forEach(child => parent.append(child));
+    /**
+     * 
+     * @param {*} user 
+     */
+    const createCard = (user) => {
+        const $card = $('<div>').addClass('card');
+        $('#gallery').append($card);
+        const $imgContain = $('<div>').addClass('card-img-container');
+        const $infoContain = $('<div>').addClass('card-info-container');
+        appendMultiples($card, $imgContain, $infoContain);
+        const $cardImg = $('<img>').addClass('card-img').attr('src', user.picture.large).attr('alt', "profile picture");
+        $imgContain.append($cardImg);
+        const $h3Info = $('<h3>').attr('id', `${user.name.first}-${user.name.last}`).addClass('card-name cap').text(`${user.name.first} ${user.name.last}`);
+        const $emailPar = $('<p>').addClass('card-text').text(user.email);
+        const $locationPar = $('<p>').addClass('card-text cap').text(`${user.location.city}, ${user.location.state}`);
+        appendMultiples($infoContain, $h3Info, $emailPar, $locationPar);
+    }
+    /**
+     * 
+     */
+    const createModalContainer = () => {
+        const $div = $('<div>').addClass('modal-container').css('display', "none");
+        const $modal = $('<div>').addClass('modal');
+        $div.append($modal);
+        const $button = $('<button>').attr('type', "button").attr('id', "modal-close-btn").addClass('modal-close-btn');
+        $modal.append($button);
+        const $strong = $('<strong>').text('X');
+        $button.append($strong);
+        $('script:first').before($div);
+    }
+    /**
+     * 
+     * @param {*} user 
+     */
+    const createModalContent = (user) => {
+        const $modalContainer = $('<div>').addClass('modal-info-container').css('display', "none");
+        const $modalImg = $('<img>').addClass('modal-img').attr('src', user.picture.large).attr('alt', "profile picture");
+        const $modalh3 = $('<h3>').attr('id', `${user.name.first}-${user.name.last}`).addClass('modal-name cap').text(`${user.name.first} ${user.name.last}`);
+        const $modalEmailPar = $('<p>').addClass('modal-text').text(user.email);
+        const $modalCityPar = $('<p>').addClass('modal-text cap').text(user.location.city);
+        const $modalhr = $('<hr>');
+        const $modalPhonePar = $('<p>').addClass('modal-text').text(user.phone);
+        const $modalLocationPar = $('<p>').addClass('modal-text').text(`${user.location.street}, ${user.location.city}, ${user.location.state} ${user.location.postcode}`);
+        const $modalDOBPar = $('<p>').addClass('modal-text').text(`Birthday: ${user.dob.date}`);
+        appendMultiples($modalContainer, $modalImg, $modalh3, $modalEmailPar, $modalCityPar, $modalhr, $modalPhonePar, $modalLocationPar, $modalDOBPar);
+        $('.modal').append($modalContainer);
+    }
+    /**
+     * 
+     */
+    const createModalBtns = () => {
+        const $modalBtn = $('<div>').addClass('modal-btn-container');
+        const $prevButton = $('<button>').attr('type', "button").attr('id', "modal-prev").addClass('modal-prev btn').text("Prev");
+        const $nextButton = $('<button>').attr('type', "button").attr('id', "modal-next").addClass('modal-next btn').text("Next");
+        appendMultiples($modalBtn, $prevButton, $nextButton);
+        $('.modal-container').append($modalBtn);
+    }
+    /**
+     * 
+     * @param {*} list 
+     * @param {*} ordinal 
+     * @param {*} number 
+     */
+    const switchUser = (list, ordinal, number) => {
+        const $employee = list.filter(function () {return $(this).css('display') === "block"}).first();
+        const $reference = list.index($employee);
+        if (list.eq($reference).children().eq(1).text() !== list[ordinal]().children().eq(1).text()) {
+            $employee.css('display', "none");
+            list.eq($reference + number).css('display', "block");
+        }
+    }
+    /**
+     * 
+     */
+    (function createSearchBar () {
+        const $searchContainer = $('.search-container');
+        const $search = $('<form>').attr('action', "#").attr('method', "get");
+        const $searchInput = $('<input>').attr('type', "search").attr('id', "search-input").attr('class', "search-input").attr('placeholder', "Search...");
+        const $submitInput = $('<input>').attr('type', "submit").attr('value', "🔍").attr('id', "search-submit").attr('class', "search-submit");
+        $searchContainer.append($search);
+        appendMultiples($search, $searchInput, $submitInput);
+    })();
+    /**
+     * 
+     * @param {*} url 
+     * @param {*} callback 
+     */
     const getJSON = function (url, callback) {
         let xhr = new XMLHttpRequest();
         xhr.open('GET', url);
@@ -35,158 +110,77 @@
         };
         xhr.send();
     }
-
+    /**
+     * 
+     * @param {*} data 
+     * @param {*} callback 
+     */
     const generateHTML = function (data, callback) {
         data.forEach(user => {
-            const card = document.createElement('div');
-            card.className = "card";
-            document.getElementById('gallery').appendChild(card);
-            const imgContain = document.createElement('div');
-            imgContain.className = "card-img-container";
-            const infoContain = document.createElement('div');
-            infoContain.className = "card-info-container";
-            card.appendChild(imgContain);
-            card.appendChild(infoContain);
-            const cardImg = document.createElement('img');
-            imgContain.appendChild(cardImg);
-            cardImg.className = "card-img";
-            cardImg.setAttribute('src', user.picture.large);
-            cardImg.setAttribute('alt', "profile picture");
-            const h3Info = document.createElement('h3');
-            const emailPar = document.createElement('p');
-            const locationPar = document.createElement('p');
-            infoContain.appendChild(h3Info);
-            infoContain.appendChild(emailPar);
-            infoContain.appendChild(locationPar);
-            h3Info.setAttribute('id', `${user.name.first}-${user.name.last}`);
-            h3Info.className = "card-name cap";
-            h3Info.textContent = `${user.name.first} ${user.name.last}`;
-            emailPar.className = "card-text";
-            emailPar.textContent = user.email;
-            locationPar.className = "card-text cap";
-            locationPar.textContent = `${user.location.city}, ${user.location.state}`;
+            createCard(user);
         });
         /*console.log(data);*/
         return callback(data);
     }
-
+    /**
+     * 
+     * @param {*} data 
+     * @param {*} callback 
+     */
     const generateModal = function (data, callback) {
-        const div = document.createElement('div');
-        div.className = "modal-container";
-        const modal = document.createElement('div');
-        modal.className = "modal";
-        div.appendChild(modal);
-        const button = document.createElement('button');
-        modal.appendChild(button);
-        button.setAttribute('type', "button");
-        button.setAttribute('id', "modal-close-btn");
-        button.className = "modal-close-btn";
-        const strong = document.createElement('strong');
-        button.appendChild(strong);
-        strong.textContent = "X";
-        document.querySelector('body').insertBefore(div, document.querySelector('script'));
-        div.style.display = "none";
+        createModalContainer();
         data.forEach(user => {
-            const modalContainer = document.createElement('div');
-            modalContainer.className = "modal-info-container";
-            const modalImg = document.createElement('img');
-            modalContainer.appendChild(modalImg);
-            modalImg.className = "modal-img";
-            modalImg.setAttribute('src', user.picture.large);
-            modalImg.setAttribute('alt', "profile picture");
-            const modalh3 = document.createElement('h3');
-            modalContainer.appendChild(modalh3);
-            modalh3.setAttribute('id', `${user.name.first}-${user.name.last}`);
-            modalh3.className = "modal-name cap";
-            modalh3.textContent = `${user.name.first} ${user.name.last}`;
-            const modalEmailPar = document.createElement('p');
-            modalContainer.appendChild(modalEmailPar);
-            modalEmailPar.className = "modal-text";
-            modalEmailPar.textContent = user.email;
-            const modalCityPar = document.createElement('p');
-            modalContainer.appendChild(modalCityPar);
-            modalCityPar.className = "modal-text cap";
-            modalCityPar.textContent = user.location.city;
-            const modalHr = document.createElement('hr');
-            modalContainer.appendChild(modalHr);
-            const modalPhonePar = document.createElement('p');
-            modalContainer.appendChild(modalPhonePar);
-            modalPhonePar.className = "modal-text";
-            modalPhonePar.textContent = user.phone;
-            const modalLocationPar = document.createElement('p');
-            modalContainer.appendChild(modalLocationPar);
-            modalLocationPar.className = "modal-text";
-            modalLocationPar.textContent = `${user.location.street}, ${user.location.city}, ${user.location.state} ${user.location.postcode}`;
-            const modalDOBPar = document.createElement('p');
-            modalContainer.appendChild(modalDOBPar);
-            modalDOBPar.className = "modal-text";
-            modalDOBPar.textContent = `Birthday: ${user.dob.date}`;
-            document.querySelector('.modal').appendChild(modalContainer);
-            modalContainer.style.display = "none";
+            createModalContent(user);
         });
-        const modalBtn = document.createElement('div');
-        modalBtn.className = "modal-btn-container";
-        const prevButton = document.createElement('button');
-        const nextButton = document.createElement('button');
-        modalBtn.appendChild(prevButton);
-        modalBtn.appendChild(nextButton);
-        prevButton.setAttribute('type', "button");
-        prevButton.setAttribute('id', "modal-prev");
-        prevButton.className = "modal-prev btn";
-        prevButton.textContent = "Prev";
-        nextButton.setAttribute('type', "button");
-        nextButton.setAttribute('id', "modal-next")
-        nextButton.className = "modal-next btn";
-        nextButton.textContent = "Next";
-        div.appendChild(modalBtn);
+        createModalBtns();
         return callback(data);
     }
 
     getJSON("https://randomuser.me/api/?results=12", (data) => {
         generateHTML(data, (data) => {
             generateModal(data, () => {
-                document.querySelectorAll('.card').forEach(card => {
-                    card.addEventListener('click', function () {
-                        const h3 = card.children[1].children[0];
-                        const id = h3.getAttribute("id");
-                        const modals = Array.from(document.querySelectorAll('.modal-info-container h3'));
-                        for (let i = 0; i < modals.length; i++) {
-                            if (id === modals[i].getAttribute("id")) {
-                                modals[i].parentNode.style.display = "block";
-                                document.querySelector('.modal-container').style.display = "block";
-                                document.getElementById('modal-close-btn').addEventListener('click', function () {
-                                    document.querySelector('.modal-container').style.display = "none";
-                                    Array.from(document.querySelectorAll('.modal-info-container')).find(employee => employee.style.display === "block").style.display = "none";
-                                });
-                                /* document.querySelector('body').addEventListener('click', function (e) {
-                                    if (e.target === document.querySelector('.modal-container')) {
-                                        e.target.style.display = "none";
-                                        const activeModal = Array.from(document.querySelectorAll('.modal-info-container')).find(employee => employee.style.display === "block");
-                                        activeModal.style.display = "none";
-                                    }
-                                }); */
-                                document.querySelectorAll('.modal-btn-container button').forEach(button => button.addEventListener('click', function (e) {
-                                    const cards = Array.from(document.querySelectorAll('.modal-info-container')) ;
-                                    if (e.target.getAttribute('id') === "modal-prev") {
-                                        const employee = cards.find(employee => employee.style.display === "block");
-                                        const reference = cards.indexOf(employee);
-                                        if (cards[reference] !== cards[0]) {
-                                            employee.style.display = "none";
-                                            cards[reference - 1].style.display = "block";
-                                        }
-                                    } else if (e.target.getAttribute('id') === "modal-next") {
-                                        const employee = cards.find(employee => employee.style.display === "block");
-                                        const reference = cards.indexOf(employee);
-                                        if (cards[reference] !== cards[cards.length - 1]) {
-                                            employee.style.display = "none";
-                                            cards[reference + 1].style.display = "block";
-                                        }
-                                    }
-                                }, false));
-                            }
+                $('.card').on('click', function () {
+                    const $h3 = $(this).children().eq(1).children().eq(0).attr('id');
+                    const $modals = $('.modal-info-container h3');
+                    $modals.each(function () {
+                        if ($h3 === $(this).attr('id')) {
+                            $(this).parent().css('display', "block");
+                            $('.modal-container').css('display', "block");
+                            $('#modal-close-btn').on('click', function () {
+                                $('.modal-container').css('display', "none");
+                                const $active = $('.modal-info-container').filter(function () {return $(this).css('display') === "block"}).first();
+                                $active.css('display', "none");
+                            });
+                        };
+                    });
+                });
+                $('body').on('click', function (e) {
+                    if ($(e.target).attr('class') === $('.modal-container').attr('class')) {
+                        $(e.target).css('display', "none");
+                        const $active = $('.modal-info-container').filter(function () {return $(this).css('display') === "block"}).first();
+                        $active.css('display', "none");
+                    }
+                });
+                $(document).on('keydown', function (e) {
+                    if ($('.modal-container').css('display') === "block") {
+                        const $cards = $('.modal-info-container');
+                        if (e.which === 39) {
+                            switchUser($cards, "last", 1)
+                        } else if (e.which === 37) {
+                            switchUser($cards, "first", -1);
+                        }
+                    }
+                });
+                $('.modal-btn-container button').each(function () {
+                    $(this).on('click', function (e) {
+                        const $cards = $('.modal-info-container');
+                        if ($(e.target).attr('id') === "modal-prev") {
+                            switchUser($cards, "first", -1);
+                        } else if ($(e.target).attr('id') === "modal-next") {
+                            switchUser($cards, "last", 1);
                         }
                     });
-                })
-            })             
-        })        
+                });
+            });
+        });
     });
